0648777ed7924d57a753f87119e53690141399e4
Hadartalisr <43495763+Hadartalisr@users.noreply.github.com>
2024-02-03 15:02:42 +0200

    wip

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -1,5 +1,18 @@
 import numpy as np
-from scipy.io import loadmat
+# from scipy.io import loadmat
+#
+# M = loadmat('data_10D.mat')
+# print(M)
+#
+#
+#
 
-M = loadmat('data_10D.mat')
-print(M)
+a = np.array([[1, 2], [3, 4]])
+
+print(a @ a)
+
+
+def he(x):
+    return x**2
+
+print(he(2))
